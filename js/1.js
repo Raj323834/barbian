4da7e98ffaed2e28d3e2e0448058b1e6325889ba
Rahul Raj <raj28446@gmail.com>
2022-03-31 13:10:53 +0530

    hello

--- js/1.js
+++ js/1.js
@@ -1,3 +1,10 @@
 window.alert("hello welcome ");
 alert("welcome to future.... ");
 window.alert("entering in future");
+let value= prompt("enter a interger value");
+document.getElementById("result4").innerHTML = (value);
+document.getElementById("result1").innerHTML = Math.round(value);
+document.getElementById("result").innerHTML = Math.floor(value);
+document.getElementById("result2").innerHTML = Math.ceil(value);
+document.getElementById("max").innerHTML = Math.max(10,50,500);
+document.getElementById("min").innerHTML = Math.min(10,50,500);
